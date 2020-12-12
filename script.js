cfc9cd238f5466e10939062eb811a5a7f0c1216f
Kirsi Ralko <kirsiralko@gmail.com>
2020-12-12 14:20:05 -0600

    time block layout

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 //Display date at the top of the page
+
+    //Retrieve today's date from moment.js
 var today = moment().format('LLLL');
 
+    //Show formatted date on page load
 function showDate() {
     $("#currentDay").text(today);    
     console.log(today);
 };
 
 $(window).on("load", showDate());
+
+//Time block functionality
+
+    //Retrieve current hour from moment.js
+var currentHour = moment().format('hA');
+console.log(currentHour);
