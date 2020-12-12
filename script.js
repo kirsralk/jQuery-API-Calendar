0d2e05d48d317127cd5536c1f0ee6a5609668026
Kirsi Ralko <kirsiralko@gmail.com>
2020-12-12 14:32:00 -0600

    functional test code to retrieve hour from moment.js, and pseudocode for time blocks in comments

--- script.js
+++ script.js
@@ -15,4 +15,17 @@ $(window).on("load", showDate());
 
     //Retrieve current hour from moment.js
 var currentHour = moment().format('hA');
-console.log(currentHour);
+var currentHour24 = moment().format('HH');
+
+console.log("The current hour is: " + currentHour);
+console.log("The current hour in 24hr time is: " + currentHour24);
+
+function testTest() {
+    if (currentHour24 == 14){console.log("true")};
+};
+
+testTest();
+
+//for time blocks, if id=current hour, change class to present
+//if id = past hour, change class to past
+//if id = future hour, change class to future
